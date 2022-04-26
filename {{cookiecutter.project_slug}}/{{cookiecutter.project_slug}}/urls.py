a33d1a7204cfb6907707b42629cf4945aff8e01c
shinkawk <shinkawk@gmail.com>
2022-04-26 16:02:45 +0900

    PoetryとSettingsの使用を変更

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
@@ -7,17 +7,7 @@ from django.views.generic import TemplateView
 from django.contrib.auth.views import logout_then_login
 from .urlhelper import required
 
-mypage_patterns = (required(
-    login_required,
-    [
-        # path('', include('{{cookiecutter.project_slug}}.apps.accounts.mypageurls')),
-    ]
-), 'mypage')
-
 urlpatterns = [
-    # path('mypage/', include(mypage_patterns, namespace='mypage')),
-    path('',
-         TemplateView.as_view(template_name='userpage/home.html'), name='home'),
     path('accounts/logout/', logout_then_login, name='logout'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
